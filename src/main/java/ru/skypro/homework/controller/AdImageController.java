092c81a47371deb7ddbe9a67acc605ff1c625c0b
Dmitriy <dmitriyndubov@gmail.com>
2025-11-19 12:19:16 +0500

    Week 1: Added DTOs and controllers with Swagger annotations

---
+++ src/main/java/ru/skypro/homework/controller/AdImageController.java
@@ -0,0 +1,23 @@
+package ru.skypro.homework.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/ads")
+@RequiredArgsConstructor
+@Tag(name = "Ad Images", description = "Получение изображений объявлений")
+public class AdImageController {
+
+    @Operation(summary = "Получить картинку объявления по ID", description = "Возвращает изображение объявления в формате JPEG")
+    @GetMapping(
+            value = "/{id}/image",
+            produces = MediaType.IMAGE_JPEG_VALUE
+    )
+    public byte[] getImage(@PathVariable int id) {
+        return new byte[0];
+    }
+}
--- src/main/java/ru/skypro/homework/controller/AuthController.java
+++ src/main/java/ru/skypro/homework/controller/AuthController.java
@@ -1,21 +1,25 @@
 package ru.skypro.homework.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
 import ru.skypro.homework.dto.Login;
 import ru.skypro.homework.dto.Register;
 
 @CrossOrigin("http://localhost:3000")
 @RestController
+@RequiredArgsConstructor
+@Tag(name = "Аутентификация", description = "Регистрация и вход пользователей")
 public class AuthController {
 
+    @Operation(summary = "Вход пользователя", description = "Позволяет авторизовать пользователя с помощью username и password")
     @PostMapping("/login")
     public Login login(@RequestBody Login login) {
         return new Login();
     }
 
+    @Operation(summary = "Регистрация пользователя", description = "Создает нового пользователя с данными из Register DTO")
     @PostMapping("/register")
     public Register register(@RequestBody Register register) {
         return new Register();
